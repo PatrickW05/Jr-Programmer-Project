2bdb92e32719772a1db177aa6489e737e30c7129
PatrickW05 <patrickjwright05@gmail.com>
2025-08-19 17:30:30 +0100

    Added data persistence

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // Start() and Update() methods deleted - we don't need them right now
+
+    public static MainManager Instance;
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        Debug.Log("Saving colour now");
+        SaveData data = new();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+}
